dcf4d944e9c72e814facf96bfa71b984895b1b5b
K.Kimura <dotnsf@jp.ibm.com>
2017-07-31 15:03:12 +0000

    fix list

--- app.js
+++ app.js
@@ -106,10 +106,21 @@ app.post( '/file', function( req, res ){
           //. バイナリを Base64 変換
           var fileimage64 = new Buffer( fileimage ).toString( 'base64' );
 
+          //. 現在日付時刻
+          var now = new Date();
+          var y = now.getFullYear();
+          var m = now.getMonth() + 1; m = ( ( m < 10 ) ? '0' : '' ) + m;
+          var d = now.getDate(); d = ( ( d < 10 ) ? '0' : '' ) + d;
+          var h = now.getHours(); h = ( ( h < 10 ) ? '0' : '' ) + h;
+          var n = now.getMinutes(); n = ( ( n < 10 ) ? '0' : '' ) + n;
+          var s = now.getSeconds(); s = ( ( s < 10 ) ? '0' : '' ) + s;
+          var ymdhns = y + "/" + m + "/" + d + " " + h + ":" + n + ":" + s;
+
           var param = {
             _id: result,
             filename: originalname,
             filetype: filetype,
+            datetime: ymdhns,
             _attachments: {
               file: {
                 content_type: filetype,
